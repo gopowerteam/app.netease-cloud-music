b7ffa9a2b3163f79c0b4d629e504ee6e95c819eb
zhuchentong <2037630@163.com>
2019-12-17 19:17:08 +0800

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,27 @@
 import React, { Component, useState, FunctionComponent, SFC } from "react";
 
-interface Props {
-  test?: number;
-}
+// interface Props {
+//   test?: number;
+// }
 
-export default class Index extends React.Component<Props, {}> {
-  static defaultProps = {
-    test: 1
-  };
+// export default class Index extends React.Component<Props, {}> {
+//   static defaultProps = {
+//     test: 1
+//   };
 
-  public render() {
-    return <>{this.props.test}</>;
-  }
+//   public render() {
+//     return <>{this.props.test}</>;
+//   }
+// }
+
+export default function() {
+  // 声明一个新的叫做 “count” 的 state 变量
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>Click me</button>
+    </div>
+  );
 }
